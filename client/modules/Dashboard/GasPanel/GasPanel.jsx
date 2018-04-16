44c0e7d3d77800ecb35d89ebbfe05c901e06372d
Oskar Szura <oskar.szura@gmail.com>
2018-04-16 21:13:51 +0200

    HOME-20 - Implement gas detector
    * Code refactor

--- client/modules/Dashboard/GasPanel/GasPanel.jsx
+++ client/modules/Dashboard/GasPanel/GasPanel.jsx
@@ -8,7 +8,7 @@ const GasPanel = (props) => {
 
   return (<div className="presence-panel">
     <div className="presence-panel__title">
-      Gas <div className={gas ? 'led-red' : 'led-green'}></div>
+      Gas <div className={gas ? 'c-led-red' : 'c-led-green'}></div>
     </div>
   </div>);
 }
