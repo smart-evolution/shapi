a037822d863bf3a0a0433c0c04644dc866f677c3
Oskar Szura <oskar.szura@gmail.com>
2018-05-02 17:41:05 +0200

    HOME-34 - Implement sound in API
    * Adopt UI to the new values served by agents

--- client/modules/Dashboard/CurrentPanel/index.jsx
+++ client/modules/Dashboard/CurrentPanel/index.jsx
@@ -5,7 +5,7 @@ const mapStateToProps = state => {
   const { motions, gas } = state;
 
   const isMotion = _.some(_.filter(motions, m => !isNaN(Number(m))), m => m != '0');
-  const isGas = _.some(_.filter(gas, g => !isNaN(Number(g))), g => g == '0');
+  const isGas = _.some(_.filter(gas, g => !isNaN(Number(g))), g => g != '0');
 
   return {
     isMotion,
