20ee9adb202d4c1572bdb47099089b513af49e0a
Oskar Szura <oskar.szura@gmail.com>
2018-11-13 19:50:45 +0100

    HOME-79 - Replace if statements with ReactRouter in app.jsx

--- client/app.jsx
+++ client/app.jsx
@@ -2,49 +2,53 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from 'history'
 import createSagaMiddleware from 'redux-saga';
-
 import AgentsStatus from './modules/AgentsStatus';
 import agentsStatusReducer from './modules/AgentsStatus/reducers';
 import agentsStatusSagas from './modules/AgentsStatus/sagas';
-
 import Dashboard from './modules/Dashboard';
 import dashboardReducer from './modules/Dashboard/reducers';
 import dashboardSagas from './modules/Dashboard/sagas';
 
+const appContainer = document.querySelector('.js-app');
 const agentsStatusContainer = document.querySelector('.js-agents-status');
 const agentContainer = document.querySelector('.js-agent');
-
 const sagaMiddleware = createSagaMiddleware();
 
+let store;
+
 if (agentsStatusContainer) {
-  const store = createStore(
+  store = createStore(
     agentsStatusReducer,
     applyMiddleware(sagaMiddleware)
   );
 
   sagaMiddleware.run(agentsStatusSagas);
-
-  render(
-    <Provider store={store}>
-      <AgentsStatus />
-    </Provider>,
-    agentsStatusContainer
-  );
 }
 
 if (agentContainer) {
-  const store = createStore(
+  store = createStore(
     dashboardReducer,
     applyMiddleware(sagaMiddleware)
   );
 
   sagaMiddleware.run(dashboardSagas);
+}
 
+if (appContainer) {
   render(
     <Provider store={store}>
-      <Dashboard />
+      <Router history={createBrowserHistory({})}>
+        <div>
+          <Route path='/' component={AgentsStatus} />
+          <Route path='/agent/:agent' component={Dashboard} />
+        </div>
+      </Router>
     </Provider>,
-    agentContainer
+    appContainer
   );
 }
+
+
