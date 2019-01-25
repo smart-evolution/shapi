6a35c029fd60a5abc70623c770076e379db0fed2
Oskar Szura <oskar.szura@gmail.com>
2019-01-25 20:10:16 +0100

    HOMR-97 - Temperature calibration per agent

--- processes/webserver/controllers/agentedit.go
+++ processes/webserver/controllers/agentedit.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
     "net/http"
+    "github.com/smart-evolution/smarthome/datasources/persistence"
+    utl "github.com/smart-evolution/smarthome/utils"
+    "gopkg.in/mgo.v2/bson"
     "github.com/smart-evolution/smarthome/processes/webserver/controllers/utils"
     "github.com/coda-it/gowebserver/router"
     "github.com/coda-it/gowebserver/session"
@@ -9,9 +12,37 @@ import (
     "fmt"
 )
 
+type AgentConfig struct {
+    ID          bson.ObjectId 	`json:"id" bson:"_id,omitempty"`
+    AgentID     bson.ObjectId 	`json:"agentId" bson:"agentId,omitempty"`
+    TmpAdjust   string          `bson:"tmpAdjustment"`
+}
+
 // CtrAgentEdit - controller for agents list
 func CtrAgentEdit(w http.ResponseWriter, r *http.Request, opt router.UrlOptions, sm session.ISessionManager, s store.IStore) {
     utils.RenderTemplate(w, r, "agentedit", sm, s)
     agentID := opt.Params["agent"]
     fmt.Println("------------------->", agentID)
+
+    dfc := s.GetDataSource("persistence")
+
+    p, ok := dfc.(persistence.IPersistance);
+    if !ok {
+        utl.Log("Invalid store ")
+        return
+    }
+
+    var agentConfig AgentConfig
+
+    c := p.GetCollection("agentConfigs")
+    err := c.Find(bson.M{
+        "agentID": agentID,
+    }).One(&agentConfig)
+
+    if err != nil {
+        utl.Log("AgentConfig not found err=", err)
+        return
+    }
+
+    utl.Log("webserver/authenticateUser: Logged in as user", agentConfig)
 }
