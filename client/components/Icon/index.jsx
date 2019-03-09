4bc93c157381fbe9d498a89768b6a443ea4cb5a2
Oskar Szura <oskar.szura@gmail.com>
2019-03-09 14:12:18 +0100

    HOME-107 - Integrate flow-type

--- client/components/Icon/index.jsx
+++ client/components/Icon/index.jsx
@@ -1,7 +1,12 @@
+// @flow
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Icon = (props) => {
+type Props = {
+  type: string,
+  className: string,
+};
+
+const Icon = (props: Props) => {
   const { type, className } = props;
 
   const classes = `c-icon c-icon--${type} ${className}`;
@@ -9,9 +14,8 @@ const Icon = (props) => {
   return (<span className={classes} />);
 };
 
-Icon.propTypes = {
-  className: PropTypes.string,
-  type: PropTypes.string,
+Icon.defaultProps = {
+  className: '',
 };
 
 export default Icon;
