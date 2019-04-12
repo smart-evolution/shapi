a66b124a75a9037856590158325569475e1c379d
Oskar Szura <oskar.szura@gmail.com>
2019-04-12 15:29:10 +0200

    HOME-127 - Add to cli-server 'proxy' option

--- processes/cliserver/cliserver.go
+++ processes/cliserver/cliserver.go
@@ -2,8 +2,10 @@ package cliserver
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/smart-evolution/smarthome/utils"
 	"net"
+	"os"
 )
 
 func handleRequest(conn net.Conn) {
@@ -17,15 +19,54 @@ func handleRequest(conn net.Conn) {
 		return
 	}
 
-	msg := make(map[string]interface{})
+	msg := make(map[string]string)
 	json.Unmarshal(buff[:n], &msg)
 
 	cmd := msg["cmd"]
+	param := msg["param"]
+
+	fmt.Println(msg)
 
 	if cmd == "status" {
 		conn.Write([]byte("application up and running"))
+	} else if cmd == "proxy" {
+		devAddr := param
+		devConn, err := net.Dial("tcp", devAddr)
+
+		if err != nil {
+			fmt.Println("error connecting to device" + devAddr)
+			os.Exit(1)
+		}
+		_, err = devConn.Write([]byte("CMDWHO"))
+
+		resBuff := make([]byte, 512)
+		devResBuff := make([]byte, 512)
+		n, err := devConn.Read(devResBuff)
+
+		if err != nil {
+			fmt.Println("error retrieving device type")
+			os.Exit(1)
+		}
+
+		_, err = conn.Write(devResBuff[:n])
+
+		for {
+			n, _ := conn.Read(resBuff)
+			devConn.Write(resBuff[:n])
+
+			if string(resBuff[:n]) == "CMDLOK" {
+				n, err := devConn.Read(devResBuff)
+
+				if err != nil {
+					fmt.Println("RES: error reading message from device")
+					break
+				}
+
+				conn.Write(devResBuff[:n])
+			}
+		}
 	} else {
-		utils.Log("invalid cli command")
+		utils.Log("invalid cli command '" + cmd + "'")
 	}
 
 	conn.Close()
