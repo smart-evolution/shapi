7dfe216bffa141d01804b6629c31f10bc900cb8e
Oskar Szura <oskar.szura@gmail.com>
2019-07-07 21:45:04 +0200

    HOME-192 - Jeep connection lag

--- client/models/proxy/constants.js
+++ client/models/proxy/constants.js
@@ -4,4 +4,4 @@ export const STATUS_PENDING: 'pending' = 'pending';
 export const STATUS_CONNECTED: 'connected' = 'connected';
 
 export const FLAG_CONNECT: 'connect' = 'connect';
-export const FLAG_DISCONNECT: 'connect' = 'connect';
+export const FLAG_DISCONNECT: 'disconnect' = 'disconnect';
