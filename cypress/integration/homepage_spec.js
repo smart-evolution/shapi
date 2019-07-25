dfca0ef7ab9a480eb0102b5c782d2750626271b1
Oskar Szura <oskar.szura@gmail.com>
2019-07-25 17:08:22 +0200

    HOME-176 - Integrate Cypress E2E testing

--- cypress/integration/homepage_spec.js
+++
@@ -1,6 +0,0 @@
-describe('The Home Page', () => {
-  it('successfully loads', () => {
-    cy.visit('http://localhost:3222');
-    cy.screenshot();
-  })
-})
---
+++ cypress/integration/loginPage.spec.js
@@ -0,0 +1,29 @@
+describe('Login page', () => {
+  it('should render successfully', () => {
+    cy.visit('http://localhost:3222');
+    cy.screenshot();
+  });
+
+  it('should navigate to registration', () => {
+    cy.get('.tst-signup').click();
+    cy.screenshot();
+  });
+
+  it('should register successfully', () => {
+    cy.get('.tst-email').type('admin@localhost.pl');
+    cy.get('.tst-password').type('admin');
+    cy.get('.tst-register').click();
+  });
+
+  it('should navigate back to login page', () => {
+    cy.get('.tst-login').click();
+    cy.screenshot();
+  });
+
+  it('should login successfully', () => {
+    cy.get('.tst-email').type('admin@localhost.pl');
+    cy.get('.tst-password').type('admin');
+    cy.get('.tst-login').click();
+    cy.screenshot();
+  });
+});
