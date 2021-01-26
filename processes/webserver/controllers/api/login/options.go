45ed4823b77a0b682c88e15a6e0276113dc8b843
Oskar Szura <oskar.szura@gmail.com>
2021-01-26 08:38:27 +0100

    HOME-298 - Integrate authentication with SHAPI

---
+++ processes/webserver/controllers/api/login/options.go
@@ -0,0 +1,14 @@
+package login
+
+import (
+	"github.com/coda-it/gowebserver/router"
+	"github.com/coda-it/gowebserver/session"
+	"github.com/coda-it/gowebserver/store"
+	"net/http"
+)
+
+// CtrAlertsOptions - options handler
+func (c *Controller) CtrAlertsOptions(w http.ResponseWriter, r *http.Request, opt router.URLOptions, sm session.ISessionManager, s store.IStore) {
+	c.CorsHeaders(w, r)
+	return
+}
